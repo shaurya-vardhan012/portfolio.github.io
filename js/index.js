2c302d3aa2e5ea364ed54221ad89e55537ee276f
Shaurya Vardhan <78726044+shaurya-vardhan012@users.noreply.github.com>
2022-01-04 09:23:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -79,6 +79,10 @@ sr.reveal('.skills_name',{
 sr.reveal('.skills_img',{
         delay: 400
     });
+sr.reveal('.contact_subtitle',{});
+sr.reveal('.contact_text',{interval:200});
+sr.reveal('.contact_input',{delay:400});
+sr.reveal('.contact_button',{delay:600});
 
 
 
